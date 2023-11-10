c28556ebc530d0342ce3b497dc7386a4a5f46cc6
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-11-10 12:31:47 +0530

    feat: removed useless comment

--- src/routes/(private)/+layout.server.ts
+++ src/routes/(private)/+layout.server.ts
@@ -1,28 +1,10 @@
-import type { PropahServa, ServersRecord, UsersRecord } from '$lib/types/maya';
-import { error, redirect } from '@sveltejs/kit';
+import type { PropahServa, ServersRecord } from '$lib/types/maya';
+import { redirect } from '@sveltejs/kit';
 
 export const load = async({locals}) => {
   if (!locals.user) {
     throw redirect(302, '/login')
   }
-  // const findStaffedServers = async (selfId: string) => {
-  //   const serversRes = (await (await fetch(`https://api.mayabot.xyz/servers`)).json()).data.servers_list as ServersRecord
-    // const servers: PropahServa[] = Object.keys(serversRes).map((id) => ({
-    //   id,
-    //   ...serversRes[id],
-    // }));
-    // try {
-    //   const selfStaffedServers: PropahServa[] = servers.filter(async(server) => {
-    //     const staffListData = await (await fetch(`https://api.mayabot.xyz/server/${server.id}/staff`)).json()
-    //     const staffList = staffListData.data.staff_members as UsersRecord;
-    //     const doesModServer = Object.keys(staffList).includes(selfId)
-    //     return doesModServer;
-    //   })
-    //   return selfStaffedServers;
-    // } catch {
-    //   throw error(500, 'Internal Server Error')
-    // }
-  // }
   const getStaffedServers = async (selfId: string) => {
     if (!locals.user) {
       throw redirect(302, '/login')
