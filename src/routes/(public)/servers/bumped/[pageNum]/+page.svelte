d3b33f586e50b93d7b7698d5c19fc4f57ff90855
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-11-10 12:06:24 +0530

    fix: removed unused import

--- src/routes/(public)/servers/bumped/[pageNum]/+page.svelte
+++ src/routes/(public)/servers/bumped/[pageNum]/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { goto } from "$app/navigation";
 	import { page } from "$app/stores";
-	import type { PropahServa } from "$lib/types/maya";
 	import { Avatar, RadioGroup, RadioItem } from "@skeletonlabs/skeleton";
 	import { onMount } from "svelte";
   $: filter_type = 'time'
