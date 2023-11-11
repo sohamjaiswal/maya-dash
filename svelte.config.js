ce6f6af8f453259a8917101fa2f3e6ed6658c9a3
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-11-11 18:22:05 +0530

    fix: removed manual csp config (not required)

--- svelte.config.js
+++ svelte.config.js
@@ -17,13 +17,6 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		csp: {
-			directives: {
-				"connect-src": ["self", "vitals.vercel-insights.com"],
-				"script-src": ["self"],
-				"object-src": ["none"]
-			}
-		}
 	}
 };
 export default config;
